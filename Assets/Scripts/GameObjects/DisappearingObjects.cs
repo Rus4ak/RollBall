e9d63c4a5f16bf97decb6acc0ac7a617674a1a5d
Dmytro Rushchak <rus4ak331@gmail.com>
2024-03-22 21:36:48 +0200

    Added meta files

--- Assets/Scripts/GameObjects/DisappearingObjects.cs
+++ Assets/Scripts/GameObjects/DisappearingObjects.cs
@@ -5,23 +5,32 @@ public class DisappearingObjects : MonoBehaviour
     [SerializeField] private float _disaperingTime;
 
     private Material _material;
+    private Color _defaultColor;
     private Color _color = new Color(.2f, 0, 0, 1f);
     private float _elapsedTime = 0f;
+    private bool _isStart = false;
 
     private void Start()
     {
         _material = GetComponent<Renderer>().material;
+        _defaultColor = _material.color;
     }
 
-    private void OnCollisionStay(Collision collision)
+    private void Update()
     {
-        if (collision.gameObject.transform.CompareTag("Player"))
+        if (_isStart)
         {
             _elapsedTime += Time.deltaTime / _disaperingTime;
-            _material.color = Color.Lerp(_material.color, _color, _elapsedTime);
-            
+            _material.color = Color.Lerp(_defaultColor, _color, _elapsedTime);
+
             if (_elapsedTime >= 1f)
                 Destroy(gameObject);
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.transform.CompareTag("Player"))
+            _isStart = true;
     }
 }
