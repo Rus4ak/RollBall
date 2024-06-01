cb6601c1239ef722d01581bf3dd90443ee313602
Dmytro Rushchak <rus4ak331@gmail.com>
2024-06-02 01:10:07 +0300

    Added a shop menu and categories

---
+++ Assets/Scripts/UserInterface/ChoiceCategory.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChoiceCategory : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _buttons;
+    [SerializeField] private List<GameObject> _categoriesMenu;
+
+    public void ActivateCategoryMenu(GameObject choicedCategoryMenu)
+    {
+        foreach (GameObject categoryMenu in _categoriesMenu)
+        {
+            if (categoryMenu == choicedCategoryMenu)
+            {
+                categoryMenu.SetActive(true);
+            }
+            else
+            {
+                categoryMenu.SetActive(false);
+            }
+        }
+    }
+
+    public void ChangeButtonColor(GameObject choicedButton)
+    {
+        foreach (GameObject button in _buttons)
+        {
+            Image image = button.transform.GetChild(0).gameObject.GetComponent<Image>();
+            Color buttonColor = button.GetComponent<Image>().color;
+            Color imageColor = image.color;
+
+            if (button == choicedButton)
+            {
+                buttonColor.a = .7f;
+                imageColor.a = .6f;
+            }
+            else
+            {
+                buttonColor.a = 1f;
+                imageColor.a = 1f;
+            }
+
+            button.GetComponent<Image>().color = buttonColor;
+            image.color = imageColor;
+        }
+    }
+}
