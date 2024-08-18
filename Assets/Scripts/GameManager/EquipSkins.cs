89bbb08fd86d592222945b8cf16825a27ddea3f1
Dmytro Rushchak <rus4ak331@gmail.com>
2024-08-19 02:05:47 +0300

    Added Cinemachine

--- Assets/Scripts/GameManager/EquipSkins.cs
+++ Assets/Scripts/GameManager/EquipSkins.cs
@@ -5,6 +5,6 @@ public static class EquipSkins
     public static void EquipMaterials(Material ballMaterial, Material floorMaterial)
     {
         ballMaterial.color = EquippedSkins.skinMaterials["ball"].color;
-        floorMaterial.color = EquippedSkins.skinMaterials["block"].color;
+        floorMaterial.color = EquippedSkins.skinMaterials["block"].color - new Color(.15f, .15f, .15f);
     }
 }
