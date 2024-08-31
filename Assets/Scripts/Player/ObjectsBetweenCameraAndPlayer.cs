0b69629b41008e89ad4b05279c4fea2fbbd7f512
Dmytro Rushchak <rus4ak331@gmail.com>
2024-09-01 02:17:53 +0300

    Fixed a bug with translucent blocks between the player and camera

--- Assets/Scripts/Player/ObjectsBetweenCameraAndPlayer.cs
+++ Assets/Scripts/Player/ObjectsBetweenCameraAndPlayer.cs
@@ -3,18 +3,9 @@ using UnityEngine;
 public class ObjectsBetweenCameraAndPlayer : MonoBehaviour
 {
     [SerializeField] private Transform _sphere;
-    [SerializeField] private Material _transparentMaterial;
-
+    
     private Renderer _renderer;
-    private Material _material;
-    private Renderer _lastRenderer;
-    private Material _lastMaterial;
-    private Color _lastMaterialColor;
-
-    private void Start()
-    {
-        _material = _sphere.GetComponent<Renderer>().material;
-    }
+    private Color _color;
 
     void Update()
     {
@@ -26,35 +17,16 @@ public class ObjectsBetweenCameraAndPlayer : MonoBehaviour
             if (hit.collider.gameObject != _sphere.gameObject)
             {
                 _renderer = hit.collider.gameObject.GetComponent<Renderer>();
-                
-                if (_renderer == _lastRenderer)
-                    return;
 
-                _material = _renderer.material;
+                _color = _renderer.material.color;
+                _color.a = .5f;
                 
-                Color gameObjectColor = _material.color;
-
-                _lastRenderer = _renderer;
-                _lastMaterial = _material;
-                _lastMaterialColor = gameObjectColor;
-                _lastMaterialColor.a = 1f;
-                
-                _material = _transparentMaterial;
-                
-                gameObjectColor.a = .2f;
-                _material.color = gameObjectColor;
-
-                _renderer.material = _material;
+                _renderer.material.color = _color;
             }
             else
             {
-                if (_lastMaterial != null)
-                {
-                    _lastMaterial.color = _lastMaterialColor;
-                    _lastRenderer.material = _lastMaterial;
-                    _lastMaterial = null;
-                    _lastRenderer = null;
-                }
+                _color.a = 1f;
+                _renderer.material.color = _color;
             }
         }
     }
